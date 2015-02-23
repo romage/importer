c5a4281c098a4aaaff394bb40674b2b000b3bca3
romage <andrew.rome@guardian.co.uk>
2015-02-23 14:29:41 +0000

    moved the IEntity to separate project to prevent circular references. Added HomeRepository.cs and HomeService.cs

---
+++ TNS.Importer.Data/HomeRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TNS.Importer.Interfaces;
+using TNS.Importer.Models;
+
+namespace TNS.Importer.Data
+{
+    public class HomeRepository: IHomeRepository
+    {
+        IRepository<Product> _productRepo;
+        UnitOfWork _uow;
+
+        public HomeRepository()
+        {
+            ImporterDataContext _ctx = new ImporterDataContext();
+            this._productRepo = new Repository<Product>(_ctx);
+            this._uow = new UnitOfWork(_ctx);
+        }
+
+        public Product SaveProduct(Product product)
+        {
+            _productRepo.Add(product);
+            _uow.SaveChanges();
+            return product;
+        }
+    }
+}
