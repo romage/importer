42c19bf92416b3b1206001a64dfb9118a6b3e2c4
romage <andrew.rome@guardian.co.uk>
2015-02-23 14:30:15 +0000

    HomeService.cs, IHomeRepository.cs, IHomeService.cs added

---
+++ TNS.Importer.Interfaces/IHomeRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TNS.Importer.Models;
+
+namespace TNS.Importer.Interfaces
+{
+    public interface IHomeRepository
+    {
+        Product SaveProduct(Product product);
+    }
+}
---
+++ TNS.Importer.Interfaces/IHomeService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TNS.Importer.Models;
+
+namespace TNS.Importer.Interfaces
+{
+    public interface IHomeService
+    {
+        Product SaveProduct(Product product);
+    }
+}
